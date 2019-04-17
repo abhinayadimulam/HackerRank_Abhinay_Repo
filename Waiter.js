c1462b1e11eefd2d76dd21bbcdc3f4700ce0e3ef
Abhinay_Adimulam <abhinay.adimulam1997@gmail.com>
2019-04-17 14:57:45 +0530

    Added Waiter

--- Waiter.js
+++ Waiter.js
@@ -1,61 +1,79 @@
+'use strict';
+
+const fs = require('fs');
+
 process.stdin.resume();
-process.stdin.setEncoding('ascii');
+process.stdin.setEncoding('utf-8');
 
-var input_stdin = "";
-var input_stdin_array = "";
-var input_currentline = 0;
+let inputString = '';
+let currentLine = 0;
 
-process.stdin.on('data', function (data) {
-    input_stdin += data;
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
 });
 
-process.stdin.on('end', function () {
-    input_stdin_array = input_stdin.split("\n");
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(str => str.trim());
+
     main();
 });
 
 function readLine() {
-    return input_stdin_array[input_currentline++];
+    return inputString[currentLine++];
 }
 
-function getNthPrime(n) {
-    find: for (var primes = [2], i = 3, root; primes.length < n; i += 2) {
-        for (root = Math.sqrt(i), j = 0; primes[j] <= root; j++) {
-            if (i % primes[j] === 0) continue find;
+function primes(n) {
+    let primeNumbers = [2];
+    const first = (n) => {
+        if (primeNumbers.length >= n) return primeNumbers.slice(0, n + 1);
+        let value = primeNumbers[primeNumbers.length - 1] + 1;
+
+        let isPrime;
+        while (primeNumbers.length < n) {
+            isPrime = true;
+            primeNumbers.some((prime) => {
+                if (value % prime === 0) {
+                    isPrime = false;
+                    return true;
+                }
+            });
+            if (isPrime) primeNumbers.push(value);
+            value++;
         }
-        primes.push(i);
+        return primeNumbers;
     }
-    return primes[n - 1];
+    return {
+        first: first
+    }
 }
 
+/*
+ * Complete the waiter function below.
+ */
+function waiter(number, q) {
+    /*
+     * Write your code here.
+     */
+    var prime = primes().first(q);
+    var b = [];
+    var ans = [];
+    for (var i = 1; i <= q; i++) {
+        b = number.filter((e) => e % prime[i - 1] == 0);
+        ans.push(...b);
+        number = number.filter((e) => e % prime[i - 1] != 0);
+        number.reverse();
+    }
+    number.reverse();
+    ans.push(...number);
+    return ans;
+}
 function main() {
-    let [N, Q] = readLine().split(' ').map(Number);
-    let A = readLine().split(' ').map(Number);
-
-    let i = 1, B = {}, temp, val;
-    while (i <= Q) {
-        temp = A;
-        A = [];
-
-        while (temp.length > 0) {
-            val = temp.pop();
-            if (val % getNthPrime(i) == 0) {
-                (B[i] = B[i] || []).push(val);
-            } else {
-                A.push(val);
-            }
-        }
-
-        i++;
-    }
-
-    B[i] = A;
-
-    for (let k in B) {
-        if (B.hasOwnProperty(k)) {
-            while (B[k].length > 0) {
-                console.log(B[k].pop());
-            }
-        }
-    }
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+    const nq = readLine().split(' ');
+    const n = parseInt(nq[0], 10);
+    const q = parseInt(nq[1], 10);
+    const number = readLine().split(' ').map(numberTemp => parseInt(numberTemp, 10));
+    let result = waiter(number, q);
+    ws.write(result.join("\n") + "\n");
+    ws.end();
 }
